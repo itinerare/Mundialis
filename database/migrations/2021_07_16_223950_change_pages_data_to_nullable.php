b7b0ed9b4159cc1f4caa711a8fb0cb781390219e
itinerare <adestus@gmail.com>
2021-07-16 18:41:17 -0400

    fix(pages): data column not nullable

---
+++ database/migrations/2021_07_16_223950_change_pages_data_to_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePagesDataToNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+            $table->longText('data')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            //
+            $table->longText('data')->change();
+        });
+    }
+}
