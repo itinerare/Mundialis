3ce33816722150c2ddc19ca6d41f0196a163ceef
itinerare <adestus@gmail.com>
2021-07-17 12:02:06 -0400

    refactor(pages): move form builder to edit view - only used there, does not need to be an include

--- resources/views/pages/create_edit_page.blade.php
+++ resources/views/pages/create_edit_page.blade.php
@@ -29,7 +29,40 @@
     {!! Form::hidden('category_id', $category->id, ['class' => 'form-control']) !!}
 @endif
 
-@if(isset($category->template)) @include('pages.form_builder._form_builder', ['category' => $page->id ? $page->category : $category, 'page' => $page]) @endif
+@if(isset($category->subject['segments']['general properties']) && View::exists('pages.form_builder._'.$category->subject['key'].'_general'))
+    @include('pages.form_builder._'.$category->subject['key'].'_general')
+@endif
+
+<h2>Infobox</h2>
+@if(isset($category->subject['segments']['infobox']) && View::exists('pages.form_builder._'.$category->subject['key'].'_infobox'))
+    @include('pages.form_builder._'.$category->subject['key'].'_infobox')
+@endif
+
+@if(isset($category->template['infobox']))
+    @foreach($category->template['infobox'] as $fieldKey=>$field)
+        @include('pages.form_builder._field_builder', ['key' => $fieldKey, 'field' => $field])
+    @endforeach
+@else
+    <p>No infobox fields have been added to this template.</p>
+@endif
+
+<h2>Sections</h2>
+@if(isset($category->subject['segments']['sections']) && View::exists('pages.form_builder._'.$category->subject['key'].'_sections'))
+    @include('pages.form_builder._'.$category->subject['key'].'_sections')
+@endif
+
+@if(isset($category->template['sections']))
+    @foreach($category->template['sections'] as $sectionKey=>$section)
+        <h3>{{ $section['name'] }}</h3>
+        @if(isset($category->template['fields'][$sectionKey]))
+            @foreach($category->template['fields'][$sectionKey] as $fieldKey=>$field)
+                @include('pages.form_builder._field_builder', ['key' => $fieldKey, 'field' => $field])
+            @endforeach
+        @endif
+    @endforeach
+@else
+    <p>No sections have been added to this template.</p>
+@endif
 
 <div class="form-group">
     {!! Form::checkbox('is_visible', 1, $page->id ? $page->is_visible : 1, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
