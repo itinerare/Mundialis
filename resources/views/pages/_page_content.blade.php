28ee517ca867fb4e46d828a5f32df63d690e0b22
itinerare <adestus@gmail.com>
2021-07-22 22:56:23 -0400

    feat(pages): auto table of contents

--- resources/views/pages/_page_content.blade.php
+++ resources/views/pages/_page_content.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     @if($page->image || isset($page->category->template['infobox']) || (isset($page->category->subject['segments']['infobox']) && View::exists('pages.content_builder._'.$page->category->subject['key'].'_infobox')))
-        <div class="mobile-show col-lg-4 mb-2">
+        <div class="mobile-show col-lg-4 m-2">
             @include('pages.content_builder._infobox_builder')
         </div>
     @endif
     <div class="col-lg-12 col-md">
         <!-- INFOBOX -->
         @if($page->image || isset($page->category->template['infobox']) || (isset($page->category->subject['segments']['infobox']) && View::exists('pages.content_builder._'.$page->category->subject['key'].'_infobox')))
-            <div class="float-right mobile-hide" style="width:25vw;">
+            <div class="float-right mobile-hide m-2" style="width:25vw;">
                 @include('pages.content_builder._infobox_builder')
             </div>
         @endif
@@ -19,6 +19,46 @@
             @include('pages.content_builder._'.$page->category->subject['key'].'_general')
         @endif
 
+        <!-- AUTO-TOC -->
+        @if(isset($page->category->template['sections']) && count($page->category->template['sections']) >= 3)
+            <div class="card mb-2" style="width: 25vw;">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="my-auto col mobile-hide">
+                            <hr/>
+                        </div>
+                        <h5 class="text-center col-lg-auto mx-2 my-auto">Contents</h5>
+                        <div class="my-auto col mobile-hide">
+                            <hr/>
+                        </div>
+                    </div>
+                    @foreach($page->category->template['sections'] as $sectionKey=>$section)
+                        {{ $loop->iteration }}. <a href="#section-{{ $sectionKey }}">{{ $section['name'] }}</a><br/>
+                        @if(isset($page->category->template['fields'][$sectionKey]))
+                            @php $sectionLoop = $loop; @endphp
+                            @php $iteration = 0; @endphp
+                            @foreach($page->category->template['fields'][$sectionKey] as $fieldKey=>$field)
+                                @switch($field['is_subsection'])
+                                    @case(1)
+                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                        {{ $sectionLoop->iteration.'.'.$loop->iteration.'.' }} <a href="#subsection-{{ $fieldKey }}">{{ $field['label'] }}</a><br/>
+                                        @php $fieldLoop = $loop; @endphp
+                                        @php $iteration = 0; @endphp
+                                    @break
+                                    @case(2)
+                                        @php $iteration++; @endphp
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                        {{ $sectionLoop->iteration.'.'.(isset($fieldLoop) ? $fieldLoop->iteration : 0).'.'.$iteration.'.' }} <a href="#subsection-{{ $fieldKey }}">{{ $field['label'] }}</a><br/>
+                                    @break
+                                @endswitch
+                            @endforeach
+                            @php $iteration = 0; @endphp
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+        @endif
+
         <!-- MAIN CONTENT -->
         @if(isset($page->category->subject['segments']['sections']) && View::exists('pages.content_builder._'.$category->subject['key'].'_sections'))
             @include('pages.content_builder._'.$page->category->subject['key'].'_sections')
@@ -26,7 +66,7 @@
 
         @if(isset($page->category->template['sections']))
             @foreach($page->category->template['sections'] as $sectionKey=>$section)
-                <h2>{{ $section['name'] }}</h2>
+                <h2 id="section-{{ $sectionKey }}">{{ $section['name'] }}</h2>
                 @if(isset($page->category->template['fields'][$sectionKey]))
                     @foreach($page->category->template['fields'][$sectionKey] as $fieldKey=>$field)
                         @include('pages.content_builder._body_builder', ['key' => $fieldKey, 'field' => $field])
--- resources/views/pages/content_builder/_body_builder.blade.php
+++ resources/views/pages/content_builder/_body_builder.blade.php
@@ -1,4 +1,4 @@
-{!! isset($field['is_subsection']) && $field['is_subsection'] ? ($field['is_subsection'] == 1 ? '<h3>'.field['label'].'</h3>' : '<h4>'.field['label'].'</h4>') : '' !!}
+{!! isset($field['is_subsection']) && $field['is_subsection'] ? ($field['is_subsection'] == 1 ? '<h3 id="subsection-'.$key.'">'.$field['label'].'</h3>' : '<h4 id="subsection-'.$key.'">'.$field['label'].'</h4>') : '' !!}
 @if($field['type'] == 'checkbox')
     {!! isset($page->data[$key]) ? ($page->data[$key] ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') : '' !!}
 @elseif(($field['type'] == 'multiple' || $field['type'] == 'choice') && isset($field['choices']))
