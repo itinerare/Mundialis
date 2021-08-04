52e4e2e0e76c7a17cda9ae0bd0b7d3c3e2bad68e
itinerare <adestus@gmail.com>
2021-08-04 17:00:19 -0400

    feat: add general update command - this may be adjusted later, but at minimum it's a good base - update version in config to pre-release in preparation

---
+++ app/Console/Commands/UpdateMundialis.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+
+class SetupMundialis extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update-mundialis';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs general update commands.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $this->info('********************');
+        $this->info('* UPDATE MUNDIALIS *');
+        $this->info('********************'."\n");
+
+        // Check if the user has run composer and run migrations
+        $this->info('This command should be run after installing packages using composer.');
+
+        if($this->confirm('Have you run the composer install command or equivalent?')) {
+            // Run migrations
+            $this->line("\n".'Running migrations...');
+            $this->call('migrate');
+
+            // Run setup commands
+            $this->line("\n".'Updating site pages and settings...');
+            $this->call('add-site-settings');
+            $this->call('add-site-pages');
+        }
+        else $this->line('Aborting! Please run composer install and then run this command again.');
+    }
+}
