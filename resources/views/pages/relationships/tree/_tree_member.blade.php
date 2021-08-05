20f1412490c1b50a35c061be8f8508cf8e79cb40
itinerare <adestus@gmail.com>
2021-08-05 09:23:19 -0400

    feat(people): adjust ancestry styling

--- resources/views/pages/relationships/tree/_tree_member.blade.php
+++ resources/views/pages/relationships/tree/_tree_member.blade.php
@@ -1,6 +1,6 @@
-<div class="col-md text-center align-self-center">
+<div class="col-md text-center d-flex flex-column">
     @if($member['page']->personRelations('parents'))
-        <div class="row mb-2">
+        <div class="row mb-2 align-bottom">
             @foreach($member['page']->personRelations('parents') as $parent)
                 @include('pages.relationships.tree._tree_member', ['member' => $parent])
             @endforeach
@@ -9,13 +9,15 @@
             <div class="border-top-0 border border-secondary rounded-bottom mb-2" style="width:{{ 25*$member['page']->personRelations('parents')->count() }}%; margin-left:{{ (100-(25*$member['page']->personRelations('parents')->count()))/2 }}%; height:20px;"></div>
         @endif
     @endif
-    @if($member['page']->image)
-        <img src="{{ $member['page']->image->thumbnailUrl }}" style="width:100px;" class="img-thumbnail mw-100"/>
-    @else
-        <img src="{{ asset('images/logo.png') }}" style="width:100px;" class="img-thumbnail mw-100"/>
-    @endif
-    <div>
-        {!! $member['page']->displayName !!}<br/>
-        {{ $member['displayType'] }}
+    <div class="mt-auto align-bottom">
+        @if($member['page']->image)
+            <img src="{{ $member['page']->image->thumbnailUrl }}" style="width:100px;" class="img-thumbnail mw-100"/>
+        @else
+            <img src="{{ asset('images/logo.png') }}" style="width:100px;" class="img-thumbnail mw-100"/>
+        @endif
+        <div>
+            {!! $member['page']->displayName !!}<br/>
+            {{ $member['displayType'] }}
+        </div>
     </div>
 </div>
