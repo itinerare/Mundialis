4658d0556021bdff7bb8a099300eea7333276f45
itinerare <adestus@gmail.com>
2021-10-10 00:41:44 -0400

    fix(tests): error running tests on fresh DB - also re-include unit tests in phpunit config, though there are none - also ignore testing .env
    fix(tests): re-add example unit test to add dir

--- tests/Feature/accessTest.php
+++ tests/Feature/accessTest.php
@@ -20,19 +20,38 @@ class accessTest extends TestCase
     // These tests check that visitor/user access to different routes is as expected.
 
     /**
+     * Performs basic site setup/
+     * tests that it can be accessed at the most basic level
+     *
+     * @return void
+     */
+    public function test_siteSetup()
+    {
+        // First, perform basic site setup. We'll need this sooner or later.
+        $this->artisan('add-site-pages');
+        $this->artisan('add-lexicon-settings');
+        $this->artisan('copy-default-images');
+
+        // Attempt to access the site on the most basic level
+        $response = $this
+            ->get('/')
+            ->assertStatus(200);
+    }
+
+    /**
      * Test visitor access when the site is closed
      *
      * @return void
      */
     public function test_visitorCannotReadWhenClosed()
     {
-        // First, add site settings
-        // We'll need these both now and later,
-        // so it's practical to simply run the command
+        // Necessary for some reason
         $this->artisan('add-site-settings');
 
+        // Set the site to private to test
         DB::table('site_settings')->where('key', 'visitors_can_read')->update(['value' => 0]);
 
+        // Attempt page access
         $response = $this
             ->get('/misc')
             ->assertStatus(302);
@@ -45,8 +64,13 @@ class accessTest extends TestCase
      */
     public function test_visitorCanReadWhenOpen()
     {
+        // Necessary for some reason
+        $this->artisan('add-site-settings');
+
+        // Set the site to open to test
         DB::table('site_settings')->where('key', 'visitors_can_read')->update(['value' => 1]);
 
+        // Attempt page access
         $response = $this
             ->get('/misc')
             ->assertStatus(200);
@@ -95,6 +119,7 @@ class accessTest extends TestCase
      */
     public function test_userCanGetUserSettings()
     {
+        // Make a temporary user
         $user = User::factory()->make();
 
         $response = $this->actingAs($user)
@@ -109,6 +134,7 @@ class accessTest extends TestCase
      */
     public function test_userCannotGetWrite()
     {
+        // Make a temporary user
         $user = User::factory()->make();
 
         $response = $this->actingAs($user)
@@ -123,6 +149,7 @@ class accessTest extends TestCase
      */
     public function test_userCannotGetAdminIndex()
     {
+        // Make a temporary user
         $user = User::factory()->make();
 
         $response = $this->actingAs($user)
@@ -137,6 +164,7 @@ class accessTest extends TestCase
      */
     public function test_editorCanGetWrite()
     {
+        // Make a temporary user
         $user = User::factory()->editor()->make();
 
         $response = $this->actingAs($user)
@@ -151,6 +179,7 @@ class accessTest extends TestCase
      */
     public function test_editorCannotGetAdminIndex()
     {
+        // Make a temporary user
         $user = User::factory()->editor()->make();
 
         $response = $this->actingAs($user)
@@ -165,6 +194,7 @@ class accessTest extends TestCase
      */
     public function test_adminCanGetWrite()
     {
+        // Make a temporary user
         $user = User::factory()->admin()->make();
 
         $response = $this->actingAs($user)
@@ -179,6 +209,7 @@ class accessTest extends TestCase
      */
     public function test_adminCanGetAdminIndex()
     {
+        // Make a temporary user
         $user = User::factory()->admin()->make();
 
         // Try to access admin dashboard
