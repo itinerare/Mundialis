2e1e89283bce4490227996d91b208f61d506402c
itinerare <ad@itinerare.net>
2021-11-13 14:32:39 -0500

    fix(users): 2FA confirm/disable functions absent

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -13,7 +13,7 @@ use App\Models\User\Rank;
 use App\Models\User\WatchedPage;
 use App\Models\User\UserUpdateLog;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
+use Laravel\Fortify\Contracts\TwoFactorAuthenticationProvider;
 
 class UserService extends Service
 {
@@ -155,6 +155,62 @@ class UserService extends Service
             $user->save();
 
             return $this->commitReturn($avatar);
+        } catch(\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+        return $this->rollbackReturn(false);
+    }
+
+    /**
+     * Confirms a user's two-factor auth.
+     *
+     * @param  string                 $code
+     * @param  array                  $data
+     * @param  \App\Models\User       $user
+     * @return bool
+     */
+    public function confirmTwoFactor($code, $data, $user)
+    {
+        DB::beginTransaction();
+
+        try {
+            if(app(TwoFactorAuthenticationProvider::class)->verify(decrypt($data['two_factor_secret']), $code['code'])) {
+                $user->forceFill([
+                    'two_factor_secret' => $data['two_factor_secret'],
+                    'two_factor_recovery_codes' => $data['two_factor_recovery_codes'],
+                ])->save();
+            }
+            else throw new \Exception('Provided code was invalid.');
+
+            return $this->commitReturn(true);
+        } catch(\Exception $e) {
+            $this->setError('error', $e->getMessage());
+        }
+        return $this->rollbackReturn(false);
+
+    }
+
+    /**
+     * Disables a user's two-factor auth.
+     *
+     * @param  string                 $code
+     * @param  \App\Models\User       $user
+     * @return bool
+     */
+    public function disableTwoFactor($code, $user)
+    {
+        DB::beginTransaction();
+
+        try {
+            if(app(TwoFactorAuthenticationProvider::class)->verify(decrypt($user->two_factor_secret), $code['code'])) {
+                $user->forceFill([
+                    'two_factor_secret' => null,
+                    'two_factor_recovery_codes' => null,
+                ])->save();
+            }
+            else throw new \Exception('Provided code was invalid.');
+
+            return $this->commitReturn(true);
         } catch(\Exception $e) {
             $this->setError('error', $e->getMessage());
         }
