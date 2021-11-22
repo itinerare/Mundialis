c58709213ae51334794608d06eea2e2e1c5d8983
itinerare <ad@itinerare.net>
2021-11-21 20:47:39 -0500

    feat(tests): add subject category view tests - includes lexicon categories, time chronologies

--- tests/Feature/SubjectTest.php
+++ tests/Feature/SubjectTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Subject\LexiconCategory;
+use App\Models\Subject\SubjectCategory;
+use App\Models\Subject\TimeChronology;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,10 +14,6 @@ use App\Models\User\User;
 class SubjectTest extends TestCase
 {
     use RefreshDatabase;
-
-    /******************************************************************************
-        SUBJECTS
-    *******************************************************************************/
 
     /**
      * Test people access.
@@ -107,6 +106,60 @@ class SubjectTest extends TestCase
     }
 
     /**
+     * Test time & events access with a chronology.
+     *
+     * @return void
+     */
+    public function test_canGetTimeWithChronology()
+    {
+        $user = User::factory()->make();
+
+        TimeChronology::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/time');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test time chronology access.
+     *
+     * @return void
+     */
+    public function test_canGetTimeChronology()
+    {
+        $user = User::factory()->make();
+
+        $chronology = TimeChronology::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/time/chronologies/'.$chronology->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test time chronology access with a child.
+     *
+     * @return void
+     */
+    public function test_canGetTimeChronologyWithChild()
+    {
+        $user = User::factory()->make();
+
+        for($i = 1; $i <= 2; $i++)
+            $chronology[$i] = TimeChronology::factory()->create();
+
+        $chronology[2]->update(['parent_id', $chronology[1]->id]);
+
+        $response = $this->actingAs($user)
+            ->get('/time/chronologies/'.$chronology[1]->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
      * Test language access.
      *
      * @return void
@@ -122,6 +175,60 @@ class SubjectTest extends TestCase
     }
 
     /**
+     * Test miscellaneous access with a category.
+     *
+     * @return void
+     */
+    public function test_canGetLanguageWithLexiconCategory()
+    {
+        $user = User::factory()->make();
+
+        LexiconCategory::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/language');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test miscellaneous category access.
+     *
+     * @return void
+     */
+    public function test_canGetLexiconCategory()
+    {
+        $user = User::factory()->make();
+
+        $category = LexiconCategory::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/language/lexicon/'.$category->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test miscellaneous category access with a child.
+     *
+     * @return void
+     */
+    public function test_canGetLexiconCategoryWithChild()
+    {
+        $user = User::factory()->make();
+
+        for($i = 1; $i <= 2; $i++)
+            $category[$i] = LexiconCategory::factory()->create();
+
+        $category[2]->update(['parent_id', $category[1]->id]);
+
+        $response = $this->actingAs($user)
+            ->get('/language/lexicon/'.$category[1]->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
      * Test miscellaneous access.
      *
      * @return void
@@ -132,6 +239,60 @@ class SubjectTest extends TestCase
 
         $response = $this->actingAs($user)
             ->get('/misc');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test miscellaneous access with a category.
+     *
+     * @return void
+     */
+    public function test_canGetMiscellaneousWithCategory()
+    {
+        $user = User::factory()->make();
+
+        SubjectCategory::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/misc');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test miscellaneous category access.
+     *
+     * @return void
+     */
+    public function test_canGetMiscellaneousCategory()
+    {
+        $user = User::factory()->make();
+
+        $category = SubjectCategory::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/misc/categories/'.$category->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test miscellaneous category access with a child.
+     *
+     * @return void
+     */
+    public function test_canGetMiscellaneousCategoryWithChild()
+    {
+        $user = User::factory()->make();
+
+        for($i = 1; $i <= 2; $i++)
+            $category[$i] = SubjectCategory::factory()->create();
+
+        $category[2]->update(['parent_id', $category[1]->id]);
+
+        $response = $this->actingAs($user)
+            ->get('/misc/categories/'.$category[1]->id);
 
         $response->assertStatus(200);
     }
