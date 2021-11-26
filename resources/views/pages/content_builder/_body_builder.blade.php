04d14371e84e5cce2c9fe85de4fdfd6f3b2bb09c
itinerare <ad@itinerare.net>
2021-11-26 16:18:48 -0500

    fix(pages): multiple choice errors; fixes #28
    - fix errors editing pages with multiple choice field info
    - fix multiple choice field display

--- resources/views/pages/content_builder/_body_builder.blade.php
+++ resources/views/pages/content_builder/_body_builder.blade.php
@@ -3,8 +3,8 @@
     {!! isset($data[$key]) ? ($data[$key] ? '<i class="fas fa-check text-success"></i>' : '<i class="fas fa-times text-danger"></i>') : '' !!}
 @elseif(($field['type'] == 'multiple' || $field['type'] == 'choice') && isset($field['choices']))
     @if($field['type'] == 'multiple')
-        @foreach($data[$key] as $answer)
-            {{ isset($field['choices'][$answer]) ? $field['choices'][$answer] : $answer }}{{ !$loop->last ? ',' : '' }}
+        @foreach($data[$key] as $choiceKey=>$answer)
+            {{ isset($field['choices'][$choiceKey]) ? $field['choices'][$choiceKey] : $answer }}{{ !$loop->last ? ',' : '' }}
         @endforeach
     @else
         {{ isset($data[$key]) ? $field['choices'][$data[$key]] : $data[$key] }}
