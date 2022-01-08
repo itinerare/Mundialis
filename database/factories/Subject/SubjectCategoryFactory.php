f75ad9c8524bd804e0abd3c6925ac75497f57c2c
itinerare <ad@itinerare.net>
2022-01-08 15:46:21 -0500

    feat(tests): infobox page edit field tests - includes subject category factory support for infobox fields

--- database/factories/Subject/SubjectCategoryFactory.php
+++ database/factories/Subject/SubjectCategoryFactory.php
@@ -55,4 +55,21 @@ class SubjectCategoryFactory extends Factory
             ];
         });
     }
+
+    /**
+     * Generate a category with just a specific infobox field.
+     *
+     * @param  string                    $key
+     * @param  string                    $type
+     * @param  string                    $rules
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function infoboxField($key = 'test', $label = 'Test', $type = 'text', $rules = null, $choices = null, $value = null)
+    {
+        return $this->state(function (array $attributes) use ($key, $label, $type, $rules, $choices, $value) {
+            return [
+                'data' => '{"infobox":{"'.$key.'":{"label":"'.$label.'","type":"'.$type.'","rules":'.($rules ? '"'.$rules.'"' : 'null').',"choices":'.($choices ? $choices : 'null').',"value":'.($value ? '"'.$value.'"' : 'null').',"help":null}}}'
+            ];
+        });
+    }
 }
