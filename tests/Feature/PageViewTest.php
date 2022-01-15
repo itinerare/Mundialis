47797dde1e90e03c8886da611caa7e21a15dd384
itinerare <ad@itinerare.net>
2022-01-15 12:03:12 -0500

    feat(tests): add subject-specific page view tests; closes #45

--- tests/Feature/PageViewTest.php
+++ tests/Feature/PageViewTest.php
@@ -99,4 +99,158 @@ class PageViewTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Test page access in the "people" subject.
+     *
+     * @return void
+     */
+    public function test_canGetPeoplePage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('people')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "places" subject.
+     *
+     * @return void
+     */
+    public function test_canGetPlacesPage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('places')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "flora and fauna" subject.
+     *
+     * @return void
+     */
+    public function test_canGetSpeciesPage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('species')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "things" subject.
+     *
+     * @return void
+     */
+    public function test_canGetThingsPage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('things')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "concepts" subject.
+     *
+     * @return void
+     */
+    public function test_canGetConceptsPage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('concepts')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "time" subject.
+     *
+     * @return void
+     */
+    public function test_canGetTimePage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('time')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test page access in the "language" subject.
+     *
+     * @return void
+     */
+    public function test_canGetLanguagePage()
+    {
+        // Create a category for the page to go into
+        $category = SubjectCategory::factory()->subject('language')->create();
+
+        // Create a page to view & version
+        $page = Page::factory()->category($category->id)->create();
+        $version = PageVersion::factory()->page($page->id)
+            ->testData($page->title, $page->summary, null, null, null)
+            ->user(User::factory()->editor()->create()->id)->create();
+
+        $response = $this->actingAs(User::factory()->make())
+            ->get('/pages/'.$page->id.'.'.$page->slug);
+
+        $response->assertStatus(200);
+    }
 }
