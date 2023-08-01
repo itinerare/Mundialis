051666cad6b879ffdff23c5d774edf1a03dede0a
itinerare <ad@itinerare.net>
2023-08-01 10:11:32 -0400

    feat(tests): add bool sequence function to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,27 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
     use CreatesApplication;
+
+    protected function setUp(): void {
+        parent::setUp();
+    }
+
+    /**
+     * Returns all possible combinations of a given number of bools.
+     * From https://stackoverflow.com/questions/29996895/list-all-combination-possible-in-a-php-array-of-boolean-value-in-php.
+     *
+     * @param int $length
+     *
+     * @return array
+     */
+    public function booleanSequences($length) {
+        $totalCombos = pow(2, $length);
+
+        $sequences = [];
+        for ($x = 0; $x < $totalCombos; $x++) {
+            $sequences[$x] = str_split(str_pad(decbin($x), $length, 0, STR_PAD_LEFT));
+        }
+
+        return $sequences;
+    }
 }
