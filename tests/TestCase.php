98af6bf9f5f1315bdad721031b71a43eecf2cd5e
itinerare <ad@itinerare.net>
2023-08-01 11:06:30 -0400

    feat(tests): perform basic site setup in TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\User\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
@@ -9,6 +10,14 @@ abstract class TestCase extends BaseTestCase {
 
     protected function setUp(): void {
         parent::setUp();
+
+        // Perform basic site setup
+        $this->artisan('add-site-settings');
+        $this->artisan('add-site-pages');
+        $this->artisan('copy-default-images');
+
+        // Create a user to assist with general testing
+        $this->user = User::factory()->make();
     }
 
     /**
