1ac9598d5f67b8811a9c163b24a3b2929098eaf9
itinerare <ad@itinerare.net>
2024-02-06 12:16:37 -0500

    fix(tests): remove boolean sequences function from test case - data providers now being static renders it irrelevant

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,23 +18,4 @@ abstract class TestCase extends BaseTestCase {
         // Create a temporary user to assist with general testing
         $this->user = User::factory()->make();
     }
-
-    /**
-     * Returns all possible combinations of a given number of bools.
-     * From https://stackoverflow.com/questions/29996895/list-all-combination-possible-in-a-php-array-of-boolean-value-in-php.
-     *
-     * @param int $length
-     *
-     * @return array
-     */
-    public function booleanSequences($length) {
-        $totalCombos = pow(2, $length);
-
-        $sequences = [];
-        for ($x = 0; $x < $totalCombos; $x++) {
-            $sequences[$x] = str_split(str_pad(decbin($x), $length, 0, STR_PAD_LEFT));
-        }
-
-        return $sequences;
-    }
 }
