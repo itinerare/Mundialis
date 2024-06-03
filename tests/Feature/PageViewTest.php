d4ff6aa3739e643c304df16b8cabdc973c4d5d82
itinerare <ad@itinerare.net>
2024-06-03 12:18:18 -0400

    feat(tests): add visibility checks to page view tests

--- tests/Feature/PageViewTest.php
+++ tests/Feature/PageViewTest.php
@@ -13,6 +13,12 @@ use Tests\TestCase;
 class PageViewTest extends TestCase {
     use RefreshDatabase, WithFaker;
 
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->editor = User::factory()->editor()->create();
+    }
+
     /**
      * Test page access.
      *
@@ -20,18 +26,22 @@ class PageViewTest extends TestCase {
      *
      * @param string $subject
      * @param bool   $withPage
+     * @param bool   $asEditor
+     * @param bool   $isVisible
      * @param int    $status
      */
-    public function testGetPage($subject, $withPage, $status) {
+    public function testGetPage($subject, $withPage, $asEditor, $isVisible, $status) {
         if ($withPage) {
             $category = SubjectCategory::factory()->subject($subject)->create();
 
-            $page = Page::factory()->category($category->id)->create();
+            $page = Page::factory()->category($category->id)->create([
+                'is_visible' => $isVisible,
+            ]);
             PageVersion::factory()->page($page->id)
                 ->user(User::factory()->editor()->create()->id)->create();
         }
 
-        $response = $this->actingAs($this->user)
+        $response = $this->actingAs($asEditor ? $this->editor : $this->user)
             ->get('/pages/'.($withPage ? $page->id.'.'.$page->slug : '9999.'.$this->faker->unique()->domainWord()));
 
         $response->assertStatus($status);
@@ -39,15 +49,31 @@ class PageViewTest extends TestCase {
 
     public static function getPageProvider() {
         return [
-            'person page'       => ['people', 1, 200],
-            'place page'        => ['places', 1, 200],
-            'species page'      => ['species', 1, 200],
-            'thing page'        => ['things', 1, 200],
-            'concept page'      => ['concepts', 1, 200],
-            'event page'        => ['time', 1, 200],
-            'language page'     => ['language', 1, 200],
-            'misc with page'    => ['misc', 1, 200],
-            'misc without page' => ['misc', 0, 404],
+            'person page'                      => ['people', 1, 0, 1, 200],
+            'person page, hidden'              => ['people', 1, 0, 0, 404],
+            'person page, hidden, as editor'   => ['people', 1, 1, 0, 200],
+            'place page'                       => ['places', 1, 0, 1, 200],
+            'place page, hidden'               => ['places', 1, 0, 0, 404],
+            'place page, hidden, as editor'    => ['places', 1, 1, 0, 200],
+            'species page'                     => ['species', 1, 0, 1, 200],
+            'species page, hidden'             => ['species', 1, 0, 0, 404],
+            'species page, hidden, as editor'  => ['species', 1, 1, 0, 200],
+            'thing page'                       => ['things', 1, 0, 1, 200],
+            'thing page, hidden'               => ['things', 1, 0, 0, 404],
+            'thing page, hidden, as editor'    => ['things', 1, 1, 0, 200],
+            'concept page'                     => ['concepts', 1, 0, 1, 200],
+            'concept page, hidden'             => ['concepts', 1, 0, 0, 404],
+            'concept page, hidden, as editor'  => ['concepts', 1, 1, 0, 200],
+            'event page'                       => ['time', 1, 0, 1, 200],
+            'event page, hidden'               => ['time', 1, 0, 0, 404],
+            'event page, hidden, as editor'    => ['time', 1, 1, 0, 200],
+            'language page'                    => ['language', 1, 0, 1, 200],
+            'language page, hidden'            => ['language', 1, 0, 0, 404],
+            'language page, hidden, as editor' => ['language', 1, 1, 0, 200],
+            'misc with page'                   => ['misc', 1, 0, 1, 200],
+            'misc page, hidden'                => ['misc', 1, 0, 0, 404],
+            'misc page, hidden, as editor'     => ['misc', 1, 1, 0, 200],
+            'misc without page'                => ['misc', 0, 0, 1, 404],
         ];
     }
 
@@ -58,18 +84,22 @@ class PageViewTest extends TestCase {
      *
      * @param string $subject
      * @param bool   $withPage
+     * @param bool   $asEditor
+     * @param bool   $isVisible
      * @param int    $status
      */
-    public function testGetPageHistory($subject, $withPage, $status) {
+    public function testGetPageHistory($subject, $withPage, $asEditor, $isVisible, $status) {
         if ($withPage) {
             $category = SubjectCategory::factory()->subject($subject)->create();
 
-            $page = Page::factory()->category($category->id)->create();
+            $page = Page::factory()->category($category->id)->create([
+                'is_visible' => $isVisible,
+            ]);
             PageVersion::factory()->page($page->id)
                 ->user(User::factory()->editor()->create()->id)->create();
         }
 
-        $response = $this->actingAs($this->user)
+        $response = $this->actingAs($asEditor ? $this->editor : $this->user)
             ->get('/pages/'.($withPage ? $page->id : 9999).'/history');
 
         $response->assertStatus($status);
@@ -82,18 +112,22 @@ class PageViewTest extends TestCase {
      *
      * @param string $subject
      * @param bool   $withPage
+     * @param bool   $asEditor
+     * @param bool   $isVisible
      * @param int    $status
      */
-    public function testGetPageGallery($subject, $withPage, $status) {
+    public function testGetPageGallery($subject, $withPage, $asEditor, $isVisible, $status) {
         if ($withPage) {
             $category = SubjectCategory::factory()->subject($subject)->create();
 
-            $page = Page::factory()->category($category->id)->create();
+            $page = Page::factory()->category($category->id)->create([
+                'is_visible' => $isVisible,
+            ]);
             PageVersion::factory()->page($page->id)
                 ->user(User::factory()->editor()->create()->id)->create();
         }
 
-        $response = $this->actingAs($this->user)
+        $response = $this->actingAs($asEditor ? $this->editor : $this->user)
             ->get('/pages/'.($withPage ? $page->id : 9999).'/gallery');
 
         $response->assertStatus($status);
@@ -106,18 +140,22 @@ class PageViewTest extends TestCase {
      *
      * @param string $subject
      * @param bool   $withPage
+     * @param bool   $asEditor
+     * @param bool   $isVisible
      * @param int    $status
      */
-    public function testCanGetPageLinks($subject, $withPage, $status) {
+    public function testCanGetPageLinks($subject, $withPage, $asEditor, $isVisible, $status) {
         if ($withPage) {
             $category = SubjectCategory::factory()->subject($subject)->create();
 
-            $page = Page::factory()->category($category->id)->create();
+            $page = Page::factory()->category($category->id)->create([
+                'is_visible' => $isVisible,
+            ]);
             PageVersion::factory()->page($page->id)
                 ->user(User::factory()->editor()->create()->id)->create();
         }
 
-        $response = $this->actingAs($this->user)
+        $response = $this->actingAs($asEditor ? $this->editor : $this->user)
             ->get('/pages/'.($withPage ? $page->id : 9999).'/links-here');
 
         $response->assertStatus($status);
